fd79c765ee0e143b8be87a3769366fc6e334eaa9
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-17 17:23:43 -0300

    refactor: improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -16,7 +16,10 @@ describe('FacebookApi', () => {
 
   beforeEach(() => {
     sut = new FacebookApi(httpClient, clientId, clientSecret)
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
   })
 
   test('Should get app token', async () => {
@@ -32,6 +35,23 @@ describe('FacebookApi', () => {
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/debug_token',
       params: { access_token: 'any_app_token', input_token: 'any_client_token' }
+    })
+  })
+
+  test('Should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: { fields: 'id,name,email', access_token: 'any_client_token' }
+    })
+  })
+
+  test('Should return facebook user', async () => {
+    const facebookUser = await sut.loadUser({ token: 'any_client_token' })
+    expect(facebookUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
