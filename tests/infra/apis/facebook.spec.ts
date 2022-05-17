1efb8391dcfcef6769c5dc72ae58163b668007d4
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-17 11:02:28 -0300

    feat: ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -16,17 +16,22 @@ describe('FacebookApi', () => {
 
   beforeEach(() => {
     sut = new FacebookApi(httpClient, clientId, clientSecret)
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
   })
 
   test('Should get app token', async () => {
     await sut.loadUser({ token: 'any_client_token' })
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
-      params: {
-        client_id: clientId,
-        client_secret: clientSecret,
-        grant_type: 'client_credentials'
-      }
+      params: { client_id: clientId, client_secret: clientSecret, grant_type: 'client_credentials' }
+    })
+  })
+
+  test('Should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: { access_token: 'any_app_token', input_token: 'any_client_token' }
     })
   })
 })
