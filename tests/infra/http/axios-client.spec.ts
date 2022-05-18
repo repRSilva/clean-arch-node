448e475afe77b90c6542f122a88ba3153d6655df
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-18 11:56:19 -0300

    refactor: isolate SUT creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -12,9 +12,13 @@ class AxiosHttpClient {
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
   let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
 
   beforeAll(() => {
     fakeAxios = axios as jest.Mocked<typeof axios>
+    url = 'any_url'
+    params = { any: 'any' }
   })
 
   beforeEach(() => {
@@ -23,8 +27,8 @@ describe('AxiosHttpClient', () => {
 
   describe('get', () => {
     test('Shoul call get with correct params', async () => {
-      await sut.get({ url: 'any_url', params: { any: 'any' } })
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', { params: { any: 'any' } })
+      await sut.get({ url, params })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
