b7e3feb2941e315c7dc11a8c7683668ae6bb3862
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-26 16:14:59 -0300

    feat: ensure FacebookLoginController returns 200 if authentication succeeds

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,5 +1,5 @@
 export class AccessToken {
-  constructor(private readonly value: string) {
+  constructor(readonly value: string) {
 
   }
 
--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { AccessToken } from '@/domain/models'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 type HttpResponse = {
@@ -14,7 +15,11 @@ class FacebookLoginController {
     }
 
     const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
-    return { statusCode: 401, data: result }
+    if (result instanceof AccessToken) {
+      return { statusCode: 200, data: { accessToken: result.value } }
+    } else {
+      return { statusCode: 401, data: result }
+    }
   }
 }
 
@@ -24,6 +29,7 @@ describe('FacebookLoginController', () => {
 
   beforeAll(() => {
     facebookAuth = mock()
+    facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
   })
 
   beforeEach(() => {
@@ -55,5 +61,10 @@ describe('FacebookLoginController', () => {
     facebookAuth.perform.mockResolvedValueOnce(new AuthenticationError())
     const httpResponse = await sut.handle({ token: 'any_token' })
     expect(httpResponse).toEqual({ statusCode: 401, data: new AuthenticationError() })
+  })
+
+  test('Should return 200 if authentication success', async () => {
+    const httpResponse = await sut.handle({ token: 'any_token' })
+    expect(httpResponse).toEqual({ statusCode: 200, data: { accessToken: 'any_value' } })
   })
 })
