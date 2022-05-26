772e34ce5435c693391103b3ce73ff13d4b0328d
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-26 17:20:46 -0300

    refactor: apply ok helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,5 +1,5 @@
 import { FacebookAuthentication } from '@/domain/features'
-import { HttpResponse, badRequest, unauthorized, serverError } from '@/application/helpers'
+import { HttpResponse, badRequest, unauthorized, serverError, ok } from '@/application/helpers'
 import { AccessToken } from '@/domain/models'
 import { RequiredFieldError } from '@/application/errors'
 
@@ -13,7 +13,7 @@ export class FacebookLoginController {
 
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
-        return { statusCode: 200, data: { accessToken: accessToken.value } }
+        return ok({ accessToken: accessToken.value })
       } else {
         return unauthorized()
       }
