e159ac3cff409dff99ade10cfd199d077eeaa5af
Rafael Silva <rafaelsilvamap@gmail.com>
2022-05-31 14:48:02 -0300

    refactor: transform ExpressRouter to a function

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,38 +1,40 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { mock, MockProxy } from 'jest-mock-extended'
 import { Controller } from '@/application/controllers'
-import { ExpressRouter } from '@/infra/http'
+import { adaptExpressRoute } from '@/infra/http'
 
 describe('ExpressRouter', () => {
   let req: Request
   let res: Response
+  let next: NextFunction
   let controller: MockProxy<Controller>
-  let sut: ExpressRouter
+  let sut: RequestHandler
 
   beforeEach(() => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
+    next = getMockRes().next
     controller = mock()
     controller.handle.mockResolvedValue({ statusCode: 200, data: { data: 'any_data' } })
-    sut = new ExpressRouter(controller)
+    sut = adaptExpressRoute(controller)
   })
 
   test('Should call handle with correct request', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
     expect(controller.handle).toHaveBeenCalledTimes(1)
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
   })
 
   test('Should call handle with empty request', async () => {
     const req = getMockReq()
-    await sut.adapt(req, res)
+    await sut(req, res, next)
     expect(controller.handle).toHaveBeenCalledTimes(1)
     expect(controller.handle).toHaveBeenCalledWith({})
   })
 
   test('Should respond with 200 and valid data', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.json).toHaveBeenCalledTimes(1)
@@ -41,7 +43,7 @@ describe('ExpressRouter', () => {
 
   test('Should respond with 400 and valid error', async () => {
     controller.handle.mockResolvedValue({ statusCode: 400, data: new Error('any_error') })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.status).toHaveBeenCalledWith(400)
     expect(res.json).toHaveBeenCalledTimes(1)
@@ -50,7 +52,7 @@ describe('ExpressRouter', () => {
 
   test('Should respond with 500 and valid error', async () => {
     controller.handle.mockResolvedValue({ statusCode: 500, data: new Error('any_error') })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.status).toHaveBeenCalledWith(500)
     expect(res.json).toHaveBeenCalledTimes(1)
