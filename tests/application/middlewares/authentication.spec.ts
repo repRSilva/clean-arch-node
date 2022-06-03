5b7f3ba2fc033a5669853f80cb28f6b0a5f1679a
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-03 10:27:13 -0300

    refactor: improve code quality

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -8,14 +8,18 @@ type Model = Error | { userId: string }
 class AuthenticationMiddleware {
   constructor(private readonly authorize: Authorize) { }
   async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    if (error !== undefined) return forbidden()
+    if (this.validate({ authorization }) === false) return forbidden()
     try {
       const userId = await this.authorize({ token: authorization })
       return ok({ userId })
     } catch {
       return forbidden()
     }
+  }
+
+  private validate({ authorization }: HttpRequest): Boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
   }
 }
 
