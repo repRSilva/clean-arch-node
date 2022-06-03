90f0948fb1eae86b56845e6eaa3fde1e3566961b
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-03 10:02:57 -0300

    refactor: isolate SUT creation

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -9,28 +9,23 @@ class AuthenticationMiddleware {
 }
 
 describe('AuthenticationMiddleware', () => {
-  beforeAll(() => {
-
-  })
+  let sut: AuthenticationMiddleware
 
   beforeEach(() => {
-
+    sut = new AuthenticationMiddleware()
   })
 
   test('Should return 403 if authorization is empty', async () => {
-    const sut = new AuthenticationMiddleware()
     const httpResponse = await sut.handle({ authorization: '' })
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
   })
 
   test('Should return 403 if authorization is null', async () => {
-    const sut = new AuthenticationMiddleware()
     const httpResponse = await sut.handle({ authorization: null as any })
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
   })
 
   test('Should return 403 if authorization is undefined', async () => {
-    const sut = new AuthenticationMiddleware()
     const httpResponse = await sut.handle({ authorization: undefined as any })
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
   })
