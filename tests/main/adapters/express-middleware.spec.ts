fc449d5af7aa519fbf07ef1a003b345181f692a9
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-03 14:17:29 -0300

    feat: ensure ExpressMiddleware add data to req.locals

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -6,7 +6,13 @@ import { mock, MockProxy } from 'jest-mock-extended'
 type Adapter = (middleware: Middleware) => RequestHandler
 const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
   const { statusCode, data } = await middleware.handle({ ...req.headers })
-  res.status(statusCode).json(data)
+  if (statusCode === 200) {
+    const entries = Object.entries(data).filter(entry => entry[1])
+    req.locals = { ...req.locals, ...Object.fromEntries(entries) }
+    next()
+  } else {
+    res.status(statusCode).json(data)
+  }
 }
 
 interface Middleware {
@@ -25,7 +31,7 @@ describe('ExpressMiddleware', () => {
     res = getMockRes().res
     next = getMockRes().next
     middleware = mock()
-    middleware.handle.mockResolvedValue({ statusCode: 500, data: { error: 'any_error' } })
+    middleware.handle.mockResolvedValue({ statusCode: 200, data: { prop: 'any_value', emptyProp: '', nullProp: null, undefined: undefined } })
   })
 
   beforeEach(() => {
@@ -46,10 +52,17 @@ describe('ExpressMiddleware', () => {
   })
 
   test('Should respond with correct error and statusCode', async () => {
+    middleware.handle.mockResolvedValueOnce({ statusCode: 500, data: { error: 'any_error' } })
     await sut(req, res, next)
     expect(res.status).toHaveBeenCalledWith(500)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should add valid data to req.locals', async () => {
+    await sut(req, res, next)
+    expect(req.locals).toEqual({ prop: 'any_value' })
+    expect(next).toHaveBeenCalledTimes(1)
   })
 })
