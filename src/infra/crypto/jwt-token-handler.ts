b4cbc490fce6a8cb393ae211ac3f0df324c94b25
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-03 09:38:10 -0300

    test: ensure JwtTokenHandler returns correct key

--- src/infra/crypto/jwt-token-handler.ts
+++ src/infra/crypto/jwt-token-handler.ts
@@ -1,14 +1,15 @@
 import { TokenGenerator, TokenValidator } from '@/domain/contracts/crypto'
-import { sign, verify } from 'jsonwebtoken'
+import { JwtPayload, sign, verify } from 'jsonwebtoken'
 
-export class JwtTokenHandler implements TokenGenerator {
+export class JwtTokenHandler implements TokenGenerator, TokenValidator {
   constructor(private readonly secret: string) { }
   async generateToken({ key, expirationInMs }: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = expirationInMs / 1000
     return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
   }
 
-  async validateToken({ token }: TokenValidator.Params): Promise<void> {
-    verify(token, this.secret)
+  async validateToken({ token }: TokenValidator.Params): Promise<TokenValidator.Result> {
+    const payload = verify(token, this.secret) as JwtPayload
+    return payload.key
   }
 }
--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -47,16 +47,24 @@ describe('JwtTokenHandler', () => {
   })
 
   describe('validateToken', () => {
+    let key: string
     let token: string
 
     beforeAll(() => {
+      key = 'any_key'
       token = 'any_token'
+      fakeJwt.verify.mockImplementation(() => ({ key: 'any_key' }))
     })
 
     test('Should call sign with correct params', async () => {
       await sut.validateToken({ token })
       expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
       expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
+    })
+
+    test('Should return the key used to sign', async () => {
+      const generatedKey = await sut.validateToken({ token })
+      expect(generatedKey).toBe(key)
     })
   })
 })
