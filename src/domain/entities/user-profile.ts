fe592fc0775e9cd7d1ac5c813d800f58cf36dab5
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-06 08:50:04 -0300

    feat: ensure ChangeProfilePicture returns correct data on success

--- src/domain/entities/user-profile.ts
+++ src/domain/entities/user-profile.ts
@@ -3,7 +3,7 @@ export class UserProfile {
   pictureUrl?: string
   constructor(readonly id: string) { }
 
-  setupPicture({ pictureUrl, name }: { pictureUrl?: string, name?: string }): void {
+  setPicture({ pictureUrl, name }: { pictureUrl?: string, name?: string }): void {
     this.pictureUrl = pictureUrl
     if (pictureUrl === undefined && name !== undefined) {
       const firstLetters = name.match(/\b(.)/g) ?? []
--- tests/domain/entities/user-profile.spec.ts
+++ tests/domain/entities/user-profile.spec.ts
@@ -8,32 +8,32 @@ describe('UserProfile', () => {
   })
 
   test('Should create with empty initials when pictureUrl is provided', async () => {
-    sut.setupPicture({ pictureUrl: 'any_url', name: 'any_name' })
+    sut.setPicture({ pictureUrl: 'any_url', name: 'any_name' })
     expect(sut).toEqual({ id: 'any_id', pictureUrl: 'any_url', initials: undefined })
   })
 
   test('Should create with empty initials when pictureUrl is provided', async () => {
-    sut.setupPicture({ pictureUrl: 'any_url' })
+    sut.setPicture({ pictureUrl: 'any_url' })
     expect(sut).toEqual({ id: 'any_id', pictureUrl: 'any_url', initials: undefined })
   })
 
   test('Should create initials with first letter of first and last names', async () => {
-    sut.setupPicture({ name: 'any_name any_rename' })
+    sut.setPicture({ name: 'any_name any_rename' })
     expect(sut).toEqual({ id: 'any_id', pictureUrl: undefined, initials: 'AA' })
   })
 
   test('Should create initials with first two letters of first and last names', async () => {
-    sut.setupPicture({ name: 'any_name' })
+    sut.setPicture({ name: 'any_name' })
     expect(sut).toEqual({ id: 'any_id', pictureUrl: undefined, initials: 'AN' })
   })
 
   test('Should create initials with first letter', async () => {
-    sut.setupPicture({ name: 'a' })
+    sut.setPicture({ name: 'a' })
     expect(sut).toEqual({ id: 'any_id', pictureUrl: undefined, initials: 'A' })
   })
 
   test('Should create with empty initials when name and pictureUrl are not provided', async () => {
-    sut.setupPicture({})
+    sut.setPicture({})
     expect(sut).toEqual({ id: 'any_id', pictureUrl: undefined, initials: undefined })
   })
 })
