738af5a29fe86394214c8ce0a131005c83a40573
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-06 23:46:56 -0300

    refactor: move UniqueID to its own file

--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -1,18 +1,4 @@
-import { UUIDGenerator } from '@/domain/contracts/gateways'
-
-class UniqueId implements UUIDGenerator {
-  constructor(private readonly date: Date) { }
-  uuid({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
-    const year = this.date.getFullYear()
-    const month = (this.date.getMonth() + 1).toString().padStart(2, '0')
-    const date = this.date.getDate().toString().padStart(2, '0')
-    const hours = this.date.getHours().toString().padStart(2, '0')
-    const minutes = this.date.getMinutes().toString().padStart(2, '0')
-    const seconds = this.date.getSeconds().toString().padStart(2, '0')
-
-    return `${key}_${year}${month}${date}${hours}${minutes}${seconds}`
-  }
-}
+import { UniqueId } from '@/infra/crypto'
 
 describe('UniqueId', () => {
   test('Should call uuid', () => {
