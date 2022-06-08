a56b7d5377375b6418092a29be7d42c8bfced534
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-08 13:53:58 -0300

    feat: ensure PgUserProfileRepository load user profile

--- src/infra/repos/postgres/user-profile.ts
+++ src/infra/repos/postgres/user-profile.ts
@@ -1,12 +1,22 @@
-import { SaveUserPicture } from '@/domain/contracts/repos'
+import { LoadUserProfile, SaveUserPicture } from '@/domain/contracts/repos'
 import { PgUser } from '@/infra/repos/postgres/entities'
 import { getRepository } from 'typeorm'
 
-type Input = SaveUserPicture.Input
+type SaveInput = SaveUserPicture.Input
+type LoadInput = LoadUserProfile.Input
+type LoadOutput = LoadUserProfile.Output
 
 export class PgUserProfileRepository implements SaveUserPicture {
-  async savePicture({ id, pictureUrl, initials }: Input): Promise<void> {
+  async savePicture({ id, pictureUrl, initials }: SaveInput): Promise<void> {
     const pgUserRepo = getRepository(PgUser)
     await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
+  }
+
+  async load({ id }: LoadInput): Promise<LoadOutput> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ id: parseInt(id) })
+    if (pgUser !== undefined) {
+      return { name: pgUser.name }
+    }
   }
 }
