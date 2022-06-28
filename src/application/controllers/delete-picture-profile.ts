fabf33d907a69db20e799a2821225d1579d3e24c
Rafael Silva <rafaelsilvamap@gmail.com>
2022-06-28 14:08:15 -0300

    refactor: move DeletePictureController to its own file

---
+++ src/application/controllers/delete-picture-profile.ts
@@ -0,0 +1,15 @@
+import { ChangeProfilePicture } from '@/domain/use-cases'
+import { HttpResponse, noContent } from '@/application/helpers'
+import { Controller } from '@/application/controllers'
+
+type HttpRequest = { userId: string }
+export class DeletePictureController extends Controller {
+  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {
+    super()
+  }
+
+  async perform({ userId }: HttpRequest): Promise<HttpResponse> {
+    await this.changeProfilePicture({ id: userId })
+    return noContent()
+  }
+}
