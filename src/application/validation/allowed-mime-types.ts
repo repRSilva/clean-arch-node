3ce4e769219232c3f58127e2958c5b8201438d5e
Rafael Silva <rafaelsilvamap@gmail.com>
2022-07-01 18:18:36 -0300

    refactor: move code from tests to src

---
+++ src/application/validation/allowed-mime-types.ts
@@ -0,0 +1,22 @@
+import { InvalidMimeTypeError } from '@/application/errors'
+import { Validator } from '@/application/validation'
+
+type Extension = 'png' | 'jpg'
+
+export class AllowedMimeTypes implements Validator {
+  constructor(private readonly allowed: Extension[], private readonly mimeType: string) { }
+  validate(): Error | undefined {
+    let isValid: boolean = false
+    if (this.isPng()) isValid = true
+    else if (this.isJpg()) isValid = true
+    if (!isValid) return new InvalidMimeTypeError(this.allowed)
+  }
+
+  private isPng(): boolean {
+    return this.allowed.includes('png') && this.mimeType === 'image/png'
+  }
+
+  private isJpg(): boolean {
+    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimeType)
+  }
+}
