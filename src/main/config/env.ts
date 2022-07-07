854ab5079836162e60c5f3834ef2603fe2178735
Rafael Silva <rafaelsilvamap@gmail.com>
2022-07-07 14:16:33 -0300

    test: add aws s3 integration tests

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,10 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '547112707027635',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '8e89e40cf92572352d5fe39601f7fdfa'
+  },
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? 'AKIAQVB5KCS4TOSGWRCC',
+    secret: process.env.AWS_S3_SECRET ?? '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',
+    bucket: process.env.AWS_S3_BUCKET ?? 'clean-ts-upload'
   }
 }
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -9,12 +9,12 @@ describe('FacebookApi Integration Tests', () => {
     sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
   })
 
-  test('Should return a Facebook User if token is validd', async () => {
+  test('Should return a Facebook User if token is valid', async () => {
     const fbUser = await sut.loadUser({ token: 'EAAHxmJqN8rMBAAuQdM1oSNU9tskyiDZA71wARAgRUEwFYLRtZCKbmPhuwTKqOkVHwiwDttUlzVFx8ZBmMVHbopE4c7O3Iuf6t2Q1HayqVtswWJ2NV0VlZBckKJesVW2ZBE4nMY7LLObcZC1SGotQojzv1ZBQ432fhymZAVMNvvKPIAJL35ifKe8dQHNaSPomp9gqQDShAumXqAZDZD' })
     expect(fbUser).toEqual({ facebookId: '111680431557057', email: 'clean_qeqxmfh_teste@tfbnw.net', name: 'clean arch teste' })
   })
 
-  test('Should return a Facebook User if token is validd', async () => {
+  test('Should return undefined if token is invalid', async () => {
     const fbUser = await sut.loadUser({ token: 'invalid' })
     expect(fbUser).toBeUndefined()
   })
