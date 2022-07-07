6c55366fbf9ffbeb4f9e22acc9d1e7b20210cafc
Rafael Silva <rafaelsilvamap@gmail.com>
2022-07-07 14:45:55 -0300

    fix: remove access key and create .env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,13 +1,13 @@
 export const env = {
-  port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? 'cl3@n@rch',
+  port: process.env.PORT ?? '',
+  jwtSecret: process.env.JWT_SECRET ?? '',
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '547112707027635',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '8e89e40cf92572352d5fe39601f7fdfa'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? ''
   },
   s3: {
-    accessKey: process.env.AWS_S3_ACCESS_KEY ?? 'AKIAQVB5KCS4TOSGWRCC',
-    secret: process.env.AWS_S3_SECRET ?? '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',
-    bucket: process.env.AWS_S3_BUCKET ?? 'clean-ts-upload'
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? '',
+    secret: process.env.AWS_S3_SECRET ?? '',
+    bucket: process.env.AWS_S3_BUCKET ?? ''
   }
 }
