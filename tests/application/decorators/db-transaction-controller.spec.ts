210ad3dc2d6367c6815713c9eba6ddf87d587748
Rafael Silva <rafaelsilvamap@gmail.com>
2022-07-15 13:57:22 -0300

    refactor: isolate SUT creation

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 class DbTransactionController {
   constructor(private readonly db: DbTransaction) { }
@@ -12,9 +12,18 @@ interface DbTransaction {
 }
 
 describe('DbTransactionController', () => {
+  let db: MockProxy<DbTransaction>
+  let sut: DbTransactionController
+
+  beforeAll(() => {
+    db = mock()
+  })
+
+  beforeEach(() => {
+    sut = new DbTransactionController(db)
+  })
+
   test('Should open transaction', async () => {
-    const db = mock<DbTransaction>()
-    const sut = new DbTransactionController(db)
     await sut.perform({ any: 'any' })
     expect(db.openTransaction).toHaveBeenCalledWith()
     expect(db.openTransaction).toHaveBeenCalledTimes(1)
